6caafd5f2e86202c08e364187a82b6ba5f808bb3
Helio Giroto <h@h>
2020-06-26 20:46:51 -0300

    sex jun 26 20:46:45 -03 2020

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+# salva a senha e usuário do Github no seu .gitconfig:
+echo "" >> ~/.gitconfig
+echo "[credential]" >> ~/.gitconfig
+echo "        helper = store" >> ~/.gitconfig
+
+
